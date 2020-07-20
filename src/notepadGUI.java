03a4026ab36464846ec46c95d55684325b27fe01
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-07-20 20:15:12 +0800

    whatever

--- src/notepadGUI.java
+++ src/notepadGUI.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import java.awt.event.KeyEvent;
 
 public class notepadGUI {
     private JPanel mainPanel;
     private JMenuBar mainMenu;
     private JTextArea textArea;
     private JScrollPane scrollBar;
+    private JMenu file;
+    private JMenu edit;
     private JFrame window;
     public static void main(String[] args){
         new notepadGUI();
@@ -14,6 +16,7 @@ public class notepadGUI {
     public notepadGUI(){
         createWindow();
         init();
+        menuInit();
         window.setVisible(true);
     }
 
@@ -24,11 +27,41 @@ public class notepadGUI {
     }
 
     public void init(){
+        //Create the scroll pane when it is needed
         //noinspection BoundFieldAssignment
         scrollBar = new JScrollPane(textArea,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        //Empty the border
         scrollBar.setBorder(BorderFactory.createEmptyBorder());
         window.add(scrollBar);
+    }
+
+    public void menuInit(){
+        //noinspection BoundFieldAssignment
+        mainMenu = new JMenuBar();
+
+        //noinspection BoundFieldAssignment
+        file = new JMenu("File");
+
+        JMenuItem newFile = new JMenuItem("New");
+        newFile.setMnemonic(KeyEvent.VK_N);
+        file.add(newFile);
+
+        JMenuItem openFile = new JMenuItem("Open");
+        file.add(openFile);
+
+        JMenuItem saveFile = new JMenuItem("Save");
+        file.add(saveFile);
+
+        JMenuItem saveAs = new JMenuItem("Save As");
+        file.add(saveAs);
+        mainMenu.add(file);
+
+        //noinspection BoundFieldAssignment
+        edit = new JMenu("Edit");
+        mainMenu.add(edit);
+        window.setJMenuBar(mainMenu);
     }
 }
