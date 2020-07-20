9ceb9136f45355dd4390c326d7db879c06c1d101
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-07-20 22:20:25 +0800

    Regular update

--- src/notepadFunction.java
+++ src/notepadFunction.java
@@ -1,7 +1,24 @@
+import java.awt.*;
+
 public class notepadFunction {
     notepadGUI gui;
+    String fileName;
 
-    public notepadFunction(notepadGUI gui){
+    public notepadFunction(notepadGUI gui) {
+        this.gui = gui;
+    }
 
+    public void createNewFile() {
+        gui.textArea.setText("");
+        gui.window.setTitle("Untitled New File");
+    }
+
+    public void exitApp() {
+        System.exit(0);
+    }
+
+    public void openFile() {
+        FileDialog explore = new FileDialog(gui.window, "Open File", FileDialog.LOAD);
+        explore.setVisible(true);
     }
 }
--- src/notepadGUI.java
+++ src/notepadGUI.java
@@ -1,35 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 
-public class notepadGUI {
-    private JPanel mainPanel;
-    private JMenuBar mainMenu;
-    private JTextArea textArea;
-    private JScrollPane scrollBar;
-    private JMenu file;
-    private JMenu edit;
-    private JFrame window;
+public class notepadGUI implements ActionListener {
+    public JPanel mainPanel;
+    public JMenuBar mainMenu;
+    public JTextArea textArea;
+    public JScrollPane scrollBar;
+    public JMenu file;
+    public JMenu edit;
+    public JFrame window;
 
-    public static void main(String[] args){
+    notepadFunction func = new notepadFunction(this);
+
+    public static void main(String[] args) {
         new notepadGUI();
     }
 
-    public notepadGUI(){
+    public notepadGUI() {
         createWindow();
         init();
         menuInit();
         window.setVisible(true);
     }
 
-    public void createWindow(){
+    public void createWindow() {
         window = new JFrame("Notepad");
-        window.setSize(800,600);
+        window.setSize(800, 600);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void init(){
+    public void init() {
         //Create the scroll pane when it is needed
         //noinspection BoundFieldAssignment
         scrollBar = new JScrollPane(textArea,
@@ -41,7 +44,7 @@ public class notepadGUI {
         window.add(scrollBar);
     }
 
-    public void menuInit(){
+    public void menuInit() {
         //noinspection BoundFieldAssignment
         mainMenu = new JMenuBar();
 
@@ -50,22 +53,32 @@ public class notepadGUI {
 
         JMenuItem newFile = new JMenuItem("New");
         newFile.setAccelerator(KeyStroke.getKeyStroke('N', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        newFile.addActionListener(this);
+        newFile.setActionCommand("new");
         file.add(newFile);
 
         JMenuItem openFile = new JMenuItem("Open");
         openFile.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        openFile.addActionListener(this);
+        openFile.setActionCommand("open");
         file.add(openFile);
 
         JMenuItem saveFile = new JMenuItem("Save");
         saveFile.setAccelerator(KeyStroke.getKeyStroke('S', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        saveFile.addActionListener(this);
+        saveFile.setActionCommand("save");
         file.add(saveFile);
 
         JMenuItem saveAs = new JMenuItem("Save As");
-        saveAs.setAccelerator(KeyStroke.getKeyStroke('S',Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()+ InputEvent.ALT_DOWN_MASK));
+        saveAs.setAccelerator(KeyStroke.getKeyStroke('S', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx() + InputEvent.ALT_DOWN_MASK));
+        saveAs.addActionListener(this);
+        saveAs.setActionCommand("saveAs");
         file.add(saveAs);
 
         JMenuItem exit = new JMenuItem("Exit");
         exit.setAccelerator(KeyStroke.getKeyStroke('X', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+        exit.addActionListener(this);
+        exit.setActionCommand("exit");
         file.add(exit);
         mainMenu.add(file);
 
@@ -75,5 +88,22 @@ public class notepadGUI {
 
         mainMenu.add(edit);
         window.setJMenuBar(mainMenu);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        switch (command) {
+            case "new":
+                func.createNewFile();
+                break;
+            case "open":
+                func.openFile();
+                break;
+            case "exit":
+                func.exitApp();
+                break;
+        }
     }
 }
