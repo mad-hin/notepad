722a207c986cf1adf92de2a95bc4f5cad5b04767
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-07-20 22:37:37 +0800

    Regular update

--- src/notepadFunction.java
+++ src/notepadFunction.java
@@ -1,8 +1,11 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class notepadFunction {
     notepadGUI gui;
     String fileName;
+    String fileAddress;
 
     public notepadFunction(notepadGUI gui) {
         this.gui = gui;
@@ -20,5 +23,23 @@ public class notepadFunction {
     public void openFile() {
         FileDialog explore = new FileDialog(gui.window, "Open File", FileDialog.LOAD);
         explore.setVisible(true);
+        fileName = explore.getFile();
+        fileAddress = explore.getDirectory();
+        if (fileName != null) {
+            gui.window.setTitle(fileName);
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileAddress + fileName));
+            gui.textArea.setText("");
+            String line;
+            System.out.println("File opening");
+            while ((line = reader.readLine()) != null) {
+                gui.textArea.append(line+"\n");
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("File not open");
+        }
     }
 }
