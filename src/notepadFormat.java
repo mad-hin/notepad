e96132019fe0b7ffb8a8c548b4ace1eebed8575d
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-07-21 12:43:42 +0800

    Regular update

--- src/notepadFormat.java
+++ src/notepadFormat.java
@@ -1,18 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class notepadFormat {
+public class notepadFormat implements ActionListener {
     notepadGUI gui;
+    JMenuItem fontItem;
+    float fontSize;
+
     public notepadFormat(notepadGUI gui) {
         this.gui = gui;
     }
 
-    public void textWarp(){
-        if(!gui.warpOn){
+    public void textWarp() {
+        if (!gui.warpOn) {
             gui.warpOn = true;
             gui.textArea.setLineWrap(true);
             gui.wordWarp.setText("Word Warp Off");
-        }else {
+        } else {
             gui.warpOn = false;
             gui.textArea.setLineWrap(false);
             gui.wordWarp.setText("Word Warp On");
@@ -22,16 +27,39 @@ public class notepadFormat {
     public void fontInit() {
         String[] allFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
         for (String fonts : allFonts) {
-            JMenuItem fontItem = new JMenuItem(fonts);
+            fontItem = new JMenuItem(fonts);
+            fontItem.addActionListener(this);
+            fontItem.setActionCommand(fonts);
             gui.textFont.add(fontItem);
         }
-        MenuScroller.setScrollerFor(gui.textFont,30);
+        MenuScroller.setScrollerFor(gui.textFont, 30);
         gui.textFormat.add(gui.textFont);
         gui.mainMenu.add(gui.textFormat);
     }
 
-    public void setSize(float size){
+    public void setSize(float size) {
+        fontSize = size;
         Font font = gui.textArea.getFont();
-        gui.textArea.setFont( font.deriveFont(size) );
+        gui.textArea.setFont(font.deriveFont(size));
+    }
+
+    public void setFont(String font) {
+        Font[] allFont = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        String[] allFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        int i = 0;
+        for (String fonts : allFonts) {
+            i++;
+            if (font.equals(fonts)) {
+                gui.textArea.setFont(allFont[i]);
+                setSize(fontSize);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        setFont(cmd);
     }
 }
